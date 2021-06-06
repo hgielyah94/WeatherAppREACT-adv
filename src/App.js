4f1ebd29d34bf36a596c60d0fb5d810d687c9445
Hayleigh Winpenny <hayleigh.winpenny@outlook.com>
2021-06-06 09:35:14 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import ChangeCity from "./ChangeCity";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>WEATHER APP</h1>
       <SearchEngine />
       <br />
       <ChangeCity />
-    <p>Coded by Hayleigh <br /><a href="https://github.com/hgielyah94/WeatherAppREACT-basic-">View on GitHub</a></p>
+      <footer>
+    <p>Coded by Hayleigh <br /><a href="https://github.com/hgielyah94/WeatherAppREACT-basic-" target="_blank" rel="noreferrer">Open-sourced on GitHub</a></p>
+    </footer>
+    </div>
     </div>
   );
 }
